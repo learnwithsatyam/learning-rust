4b144a490fe2ed128da1381f12df4fe5d7e8cff3
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-08 13:59:27 +0530

    simple variables in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
+
+    // simple variables 
+    let a: i8 = 4;
+    let b: i16 = 200;
+    let c: i32 = 1000;
+    let d: i64 = 100000;
+    let e: i128 = 1000000000000000000;
+
+    println!("{}", a);
+
+    // here it looks like we can redifine a variable
+    let a: i8 = 5;
+    let b: i16 = 230;
+    let c: i32 = 5000;
+    let d: i64 = 1002400;
+    let e: i128 = 1002424000000000000000;
+
+    // similarly we have bool, f8 for float 8bit , u8 for unsigned 8bit , etc
+    
+
 }
